dfb3a73f37ae561fa217c8edeb63d0544ed54c65
jennyrowlett <83476080+jennyrowlett@users.noreply.github.com>
2023-01-11 17:44:54 -0500

    1/11/2023

--- rain.m
+++ rain.m
@@ -1,4 +1,4 @@
-ncdisp('rainfiles/rain25s100e_10m.cdf')
+ncdisp('rainfiles/rain25s100e_10m.cdf');
 timeData = ncread('rainfiles/rain25s100e_10m.cdf','time');
 precLon = ncread('rainfiles/rain25s100e_10m.cdf','lon');
 precLat = ncread('rainfiles/rain25s100e_10m.cdf','lat');
@@ -13,7 +13,7 @@ precLocation = [precLon, precLat];
 %     end
 % end
 precipitationData = ncread('rainfiles/rain25s100e_10m.cdf','RN_485');
-precipitationValues = squeeze(precipitationData(:,:,1,:)); % units is mm/hr
+precipitation = squeeze(precipitationData(:,:,1,:)); % units is mm/hr
 fileName = 'rainfiles/rain25s100e_10m.cdf';
 timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
 startTime = erase(timeDescrip, 'minutes since '); % strips words
@@ -22,30 +22,60 @@ currentMonth = month(startTime);
 currentYear = year(startTime);
 adjustedTime = dateshift(startTime, 'start', 'minute', timeData); % calculate dates based on minutes
 
-timePrecipitation = struct('date', adjustedTime, 'precipitation', precipitationValues);
-precipitationByMonths = struct('month', {}, 'data',{},'average',{});
-
-newMonth = true;
-for i=1:size(precipitationValues,1)
-    if currentMonth ~= month(timePrecipitation.date(i)) 
-        currentMonth = month(timePrecipitation.date(i));
-        currentYear = year(timePrecipitation.date(i));
-        newMonth = true;
-        precipitationByMonths(end).data = monthData;
-    else
-        if newMonth == true
-            newMonth = false;
-            precipitationByMonths(end+1).month = string(month(timePrecipitation.date(i), 'shortname')) + '-' + string(year(timePrecipitation.date(i)));
-            monthData = table([timePrecipitation.date(i)],[timePrecipitation.precipitation(i)],'VariableNames',["times", "precipitation"]);
-        else
-            monthData = [monthData; {timePrecipitation.date(i), timePrecipitation.precipitation(i)}];
+tt = table(adjustedTime, precipitation); % table with times and salinity values
+%precipitationByMonths = struct('month', {}, 'data',{},'average',{});
+fiveDayStv = struct("start", {}, "interval", {},"max", {});
+newinterval = true;
+dayCounter = 0;
+for i=1:size(tt, 1)
+    if tt(i,2).precipitation < 0
+        tt(i,2).precipitation = 0;
+    end
+    if newinterval == true
+        startDate = tt(i,1).adjustedTime;
+        currentDay = day(tt(i,1).adjustedTime);
+        fiveDayTable = table([tt(i,1).adjustedTime],[tt(i,2).precipitation],'VariableNames',["days", "precipitation"]);
+        newinterval = false;
+    elseif dayCounter < 5 && currentDay == day(tt(i,1).adjustedTime)
+        fiveDayTable = [fiveDayTable; {tt(i,1).adjustedTime, tt(i,2).precipitation}];
+    elseif dayCounter < 5
+        currentDay = day(tt(i,1).adjustedTime);
+        dayCounter = dayCounter + 1;
+        if dayCounter == 5
+            dayCounter = 0;
+            fiveDayStv(end+1).start = startDate;
+            fiveDayStv(end).interval = fiveDayTable;
+            fiveDayStv(end).max = max(fiveDayTable.precipitation);
+            fiveDayTable = table([tt(i,1).adjustedTime],[tt(i,2).precipitation],'VariableNames',["days", "precipitation"]);
+            startDate = tt(i,1).adjustedTime;
         end
     end
-end
-precipitationByMonths(end).data = monthData;
-
-for j = 1:size(precipitationByMonths,2)
-    precipitationByMonths(j).average = mean(precipitationByMonths(j).data.precipitation);
-end
-
-save('rain25s100e_10m.mat', 'precipitationByMonths')
+end   
+% newMonth = true;
+% for i=1:size(precipitationValues,1)
+%     if currentMonth ~= month(timePrecipitation.date(i)) 
+%         currentMonth = month(timePrecipitation.date(i));
+%         currentYear = year(timePrecipitation.date(i));
+%         newMonth = true;
+%         precipitationByMonths(end).data = monthData;
+%     else
+%         if timePrecipitation.precipitation(i) < 0
+%             timePrecipitation.precipitation(i) = 0;
+%         end
+%         if newMonth == true
+%             newMonth = false;
+%             precipitationByMonths(end+1).month = timePrecipitation.date(i);
+%                 monthData = table([timePrecipitation.date(i)],[timePrecipitation.precipitation(i)],'VariableNames',["times", "precipitation"]);
+%         else
+%             monthData = [monthData; {timePrecipitation.date(i), timePrecipitation.precipitation(i)}];
+%         end
+%     end
+% end
+% precipitationByMonths(end).data = monthData;
+% 
+% for j = 1:size(precipitationByMonths,2)
+%     precipitationByMonths(j).average = mean(precipitationByMonths(j).data.precipitation);
+% end
+% 
+% plot(precipitationByMonths.month, precipitationByMonths.average)
+% save('rain25s100e_10m.mat', 'precipitationByMonths')
--- seasalinity.m
+++ seasalinity.m
@@ -1,57 +1,65 @@
 %rainFiles = openFiles('rain.zip','rainfiles');
-sssFiles = openFiles('sss.zip','sssfiles');
+%sssFiles = openFiles('sss.zip','sssfiles');
 
-for file = {sssFiles.name}
-    fileName =  'sssfiles\' + string(file{1});
-    timeData = ncread(fileName,'time');
-    salinityData = ncread(fileName,'S_41');
-    ssValues = salinityData(:,:,1,:); 
-    salinityValues = squeeze(ssValues); % puts salinity values in 1-D array
-    
-    timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
-    startTime = erase(timeDescrip, 'hours since '); % strips words
-    startTime = datetime(startTime);
-    startMonth = month(startTime);
-    startYear = year(startTime);
-    adjustedTime = dateshift(startTime, 'start', 'hour', timeData); % calculate dates based on hours
-    tt = timetable(adjustedTime, salinityValues); % table with times and salinity values
-    timeBetween = between(startTime,adjustedTime(end));
+%for file = {sssFiles.name}
+fileName =  'sssfiles\sss21n23w_hr.cdf'; %+string(file{1})
+timeData = ncread(fileName,'time');
+salinityData = ncread(fileName,'S_41');
+ssValues = salinityData(:,:,1,:);
+salinity = squeeze(ssValues); % puts salinity values in 1-D array
 
-    M = datetime(startYear,startMonth,1) + calmonths(0:calmonths(timeBetween)+1); % months array that starts with starting month
-    stv = struct("date",{},"stv",{}, 'interval',{});
-    salinitiesBy5Days = struct("salinities",{},'stv',{});
-    salinitiesByMonth = struct("salinities",{});
-    
-    for c=1:size(M,2)-1
-        y = year(M(1,c));
-        stringM = month(M(1,c),'name');
-        currentMonth = timerange(M(1,c),M(1,c+1)-caldays(1),'days'); % separates days into the correct month 
-        monthSalinity = tt(currentMonth,:); % makes a table with the salinity values from 'currentMonth
-        salinitiesByMonth(c).salinities = monthSalinity;
-        intervals = M(1,c) + caldays(0:5:30); % creates intervals
-        stvMonth = struct('date',{},'stv', {}); % cell array to hold standard deviations
-        counter = 1;
-        for m=1:size(intervals,2)-1 % dividing the month up into 5-day intervals
-            dayRange = timerange(intervals(1,m),intervals(1,m+1)-hours(1),'hours');
-            fiveDayInterval = monthSalinity(dayRange,:);
-            salinitiesBy5Days(end+1).salinities = fiveDayInterval;
-            if ~isnan(fiveDayInterval.salinityValues)
-                S = std(fiveDayInterval.salinityValues);
-                stvMonth(counter).stv = S;
-                stvMonth(counter).date = string(intervals(1,m)) + '-' + string(intervals(1, m+1)-days(1));
-                counter = counter + 1;
-                salinitiesBy5Days(end).stv = S;
+timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
+startTime = erase(timeDescrip, 'hours since '); % strips words
+startTime = datetime(startTime);
+startMonth = month(startTime);
+startYear = year(startTime);
+adjustedTime = dateshift(startTime, 'start', 'hour', timeData); % calculate dates based on hours
+tt = table(adjustedTime, salinity); % table with times and salinity values
+fiveDayStv = struct("start", {}, "interval", {},"stv", {});
+dayCounter = mod(day(tt(1,1).adjustedTime),5);
+newinterval = true;
+intervalCounter = 0;
+
+for i=1:size(tt, 1)
+    if newinterval == true
+        startDate = tt(i,1).adjustedTime + days(2);
+        currentDay = day(tt(i,1).adjustedTime);
+        fiveDayTable = table([tt(i,1).adjustedTime],[tt(i,2).salinity],'VariableNames',["days", "salinity"]);
+        newinterval = false;
+    elseif dayCounter < 5 && currentDay == day(tt(i,1).adjustedTime)
+        fiveDayTable = [fiveDayTable; {tt(i,1).adjustedTime, tt(i,2).salinity}];
+    elseif dayCounter < 5
+        currentDay = day(tt(i,1).adjustedTime);
+        dayCounter = dayCounter + 1;
+        if dayCounter == 5
+            dayCounter = 0;
+            fiveDayStv(end+1).start = startDate;
+           % fiveDayStv(end).end = tt(i,1).adjustedTime;
+            NaNcount = 0;
+            for j=1:size(fiveDayTable,1)
+                if isnan(fiveDayTable(j,2).salinity)
+                    NaNcount = NaNcount + 1;
+                end
             end
+            percNaN = NaNcount/size(fiveDayTable,1);
+            if percNaN > 0.20
+                fiveDayStv(end).interval = fiveDayTable;
+            else
+                fiveDayTable=fiveDayTable(~any(ismissing(fiveDayTable),2),:); %remove missing data
+                fiveDayStv(end).interval = fiveDayTable;
+            end
+            fiveDayStv(end).stv = std(fiveDayTable.salinity);
+            fiveDayTable = table([tt(i,1).adjustedTime],[tt(i,2).salinity],'VariableNames',["days", "salinity"]);
+            startDate = tt(i,1).adjustedTime;
+        else
+            fiveDayTable = [fiveDayTable; {tt(i,1).adjustedTime, tt(i,2).salinity}];
         end
-        average = mean([stvMonth.stv]);
-        stv(c).date = string(stringM) +' ' + y;
-        stv(c).stv = average;
-        stv(c).interval = stvMonth;
     end
-    fileName = 'output\'+ erase(string(file{1}),'.cdf') + '.mat';
-    save(fileName, 'stv', 'tt','salinitiesBy5Days','salinitiesByMonth')
-end
+end    
 
+plot([fiveDayStv.start], [fiveDayStv.stv])
+
+save("output\sss21n23w_hr.mat","fiveDayStv")
 
 function fileList = openFiles(folder, targetFolder)
     disp(targetFolder)
