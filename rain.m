87d2d9a41e36c530bf5759c5e8b8edd8c2d6ed97
jennyrowlett <83476080+jennyrowlett@users.noreply.github.com>
2023-01-23 12:21:19 -0500

    Update rain.m

--- rain.m
+++ rain.m
@@ -1,4 +1,3 @@
-
 %ncdisp('rainfiles/rain25s100e_10m.cdf');
 timeData = ncread('rainfiles/rain25s100e_10m.cdf','time');
 precLon = ncread('rainfiles/rain25s100e_10m.cdf','lon');
@@ -31,24 +30,19 @@ currentMonth = month(startTime);
 currentYear = year(startTime);
 
 tt = table(adjustedTime, precipitation); % table with times and salinity values
-%precipitationByMonths = struct('month', {}, 'data',{},'average',{});
 [h,m,s]= hms(startTime);
 if h ~= 0 || m ~=0 || s ~=0
     [y,m,d] = ymd(startTime);
     newStart = datetime(y,m,d+1,0,0,0);
     for i=1:size(tt,1)
         if newStart ~= tt(1,1).adjustedTime
-       %     disp('dumb')
-       %disp(tt(i,1).adjustedTime)
             tt(1,:)=[];
-            disp(tt(1,1));
         else
             break
         end
     end
 end
-
-fiveDayStv = struct("start", {}, "interval", {},"max", {});
+fiveDayMax = struct("middle", {}, "interval", {},"max", {});
 newinterval = true;
 dayCounter = 0;
 for i=1:size(tt, 1)
@@ -56,7 +50,7 @@ for i=1:size(tt, 1)
         tt(i,2).precipitation = 0;
     end
     if newinterval == true
-       % startDate = tt(i,1).adjustedTime;
+        startDate = tt(i,1).adjustedTime;
         currentDay = day(tt(i,1).adjustedTime);
         fiveDayTable = table([],[],'VariableNames',["days", "precipitation"]);
         precipitationTotal = tt(i,2).precipitation;
@@ -66,23 +60,46 @@ for i=1:size(tt, 1)
     elseif dayCounter < 5
         currentDay = day(tt(i,1).adjustedTime);
         dayCounter = dayCounter + 1;
-        fiveDayTable = [fiveDayTable; {tt(i,1).adjustedTime, precipitationTotal}];
+        fiveDayTable = [fiveDayTable; {tt(i,1).adjustedTime-days(1), precipitationTotal}];
         precipitationTotal = 0;
-        if dayCounter == 5
-            dayCounter = 0;
-            fiveDayStv(end+1).start = startDate;
-            fiveDayStv(end).interval = fiveDayTable;
-            fiveDayStv(end).max = max(fiveDayTable.precipitation);
-            %newinterval = true;
-            fiveDayTable = table([tt(i,1).adjustedTime],[tt(i,2).precipitation],'VariableNames',["days", "precipitation"]);
-            startDate = tt(i,1).adjustedTime;
+    elseif dayCounter == 5
+        dayCounter = 0;
+        fiveDayMax(end+1).middle = startDate+days(2);
+        fiveDayMax(end).interval = fiveDayTable;
+        fiveDayMax(end).max = max(fiveDayTable.precipitation);
+        precipitationTotal = precipitationTotal+ tt(i,2).precipitation;
+        fiveDayTable = table([],[],'VariableNames',["days", "precipitation"]);
+        startDate = tt(i-1,1).adjustedTime;
+    end
+end
+
+monthMax = struct("month", {}, "maxData", {}, "median", {});
+monthData = [];
+monthNames = [];
+newTable = true;
+for i=1:12
+    for j=1:size(fiveDayMax,2)
+        if isnan(fiveDayMax(1,j).max)
+            continue
+        elseif month(fiveDayMax(j).middle) == i
+            if newTable == true
+                currentMax = table([fiveDayMax(1,j).middle],[fiveDayMax(1,j).max],'VariableNames',["days", "max"]);
+                newTable = false;
+            else
+                currentMax = [currentMax; {fiveDayMax(1,j).middle,fiveDayMax(1,j).max}];
+            end
         end
     end
-end   
+    monthMax(end+1).month = datetime(2000,i,1,'format','MMM');
+    monthMax(end).maxData = currentMax;
+    monthMax(end).median = median(currentMax.max);
+    monthData = [monthData; currentMax.max];
+    monthNames = [monthNames; repmat({string(i)},size(currentMax,1),1)];
+    newTable = true;
+end
 
-%boxplot(monthData, monthNames)
-% % xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
-% % graphname = 'graphs2\'+string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
-% % savefig(graphname);
+boxplot(monthData, monthNames)
+xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
+graphname = 'graphs2\'+string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
+savefig(graphname);
 
-save('rain25s100e_10m.mat', 'fiveDayStv');
