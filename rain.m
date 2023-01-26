1aba28bad37c509370e274cea5c882b5c91bd816
jennyrowlett <83476080+jennyrowlett@users.noreply.github.com>
2023-01-26 00:02:17 -0500

    updated scripts

--- rain.m
+++ rain.m
@@ -1,98 +1,127 @@
+rainFiles = openFiles('rain.zip','rainfiles');
 
-timeData = ncread('rainfiles/rain25s100e_10m.cdf','time');
-precLon = ncread('rainfiles/rain25s100e_10m.cdf','lon');
-precLat = ncread('rainfiles/rain25s100e_10m.cdf','lat');
-precLocation = [precLon, precLat];
+for file = {rainFiles.name}
+    %fileName = 'rainfiles\rain0n35w_hr.cdf';
+    fileName = 'rainfiles\' +string(file{1});
+    timeData = ncread(fileName,'time');
+    precLon = ncread(fileName,'lon');
+    precLat = ncread(fileName,'lat');
 
-precipitationData = ncread('rainfiles/rain25s100e_10m.cdf','RN_485');
-precipitation = squeeze(precipitationData(:,:,1,:)); % units is mm/hr
-fileName = 'rainfiles/rain25s100e_10m.cdf';
-timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
-if contains(fileName, '10m')
-    startTime = erase(timeDescrip, 'minutes since '); % strips words
-    startTime = datetime(startTime);
-    adjustedTime = dateshift(startTime, 'start', 'minute', timeData); % calculate dates based on minutes
+    precipitationData = ncread(fileName,'RN_485');
+    precipitation = squeeze(precipitationData(:,:,1,:)); % units is mm/hr
+    timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
+    if contains(fileName, '10m')
+        startTime = erase(timeDescrip, 'minutes since '); % strips words
+        startTime = datetime(startTime);
+        adjustedTime = dateshift(startTime, 'start', 'minute', timeData); % calculate dates based on minutes
+        numDays = timeData(end)/(24*60);
+    else
+        startTime = erase(timeDescrip, 'hours since');
+        startTime = datetime(startTime);
+        adjustedTime = dateshift(startTime, 'start', 'hour', timeData);
+        numDays = adjustedTime(end)-adjustedTime(1);
+    end
+    currentMonth = month(startTime);
+    currentYear = year(startTime);
 
-else
-    startTime = erase(timeDescrip, 'hours since');
-    startTime = datetime(startTime);
-    adjustedTime = dateshift(startTime, 'start', 'hour', timeData); % calculate dates based on minutes
-end
-currentMonth = month(startTime);
-currentYear = year(startTime);
+    tt = timetable(adjustedTime, precipitation);
 
-tt = table(adjustedTime, precipitation); % table with times and salinity values
-[h,m,s]= hms(startTime);
-if h ~= 0 || m ~=0 || s ~=0
-    [y,m,d] = ymd(startTime);
-    newStart = datetime(y,m,d+1,0,0,0);
-    for i=1:size(tt,1)
-        if newStart ~= tt(1,1).adjustedTime
-            tt(1,:)=[];
+    intervalStart = datetime(year(adjustedTime(1)),month(adjustedTime(1)),day(adjustedTime(1))+1,0,0,0);
+    intervals = intervalStart + days(0:5:numDays);
+
+    fiveDayMax = struct("middle", {}, "intervalData",{}, "avgData",{},"max", {},"percmissing", {});
+
+    for m=1:size(intervals,2)-1 % dividing the month up into 5-day intervals
+        intervalRange = timerange(intervals(1,m),intervals(1,m+1)-caldays(1),'days');
+        fiveDayInterval = tt(intervalRange,:);
+        fiveDayInterval.precipitation(fiveDayInterval.precipitation < 0,:) = 0; %Are we setting negative values to 0 or throwing them out? Gives a different mean
+        fiveDayMax(end+1).middle = intervals(1,m) + days(2) + hours(12);
+        NaNCounter = 0;
+        for i=1:size(fiveDayInterval,1)
+            if isnan(fiveDayInterval(i,1).precipitation)
+                NaNCounter = NaNCounter + 1;
+            end
+        end
+        percNaN = NaNCounter/size(fiveDayInterval,1);
+        fiveDayMax(end).percmissing = percNaN;
+        if isempty(fiveDayInterval)
+            continue
+        end
+        if percNaN > 0.20
+            fiveDayMax(end).intervalData = fiveDayInterval;
+            fiveDayMax(end).max = 0;
         else
-            break
+            fiveDayInterval=rmmissing(fiveDayInterval); %remove missing data
+            fiveDayMax(end).intervalData = fiveDayInterval;
+            counter = 0;
+            currentDay = day(fiveDayInterval(1,1).adjustedTime);
+            dayIntervals = fiveDayInterval(1,1).adjustedTime + caldays(0:1:5);
+            fiveDayAvg = struct('day',{},'avg',{});
+            for j=1:size(dayIntervals, 2)-1
+                dayRange = timerange(dayIntervals(1,j),dayIntervals(1,j+1)-caldays(1),'days');
+                dayValues = fiveDayInterval(dayRange,:);
+                dayAvg = mean(dayValues.precipitation);
+                fiveDayAvg(end+1).day = dayIntervals(1,j);
+                fiveDayAvg(end).avg = dayAvg;
+            end
+            fiveDayMax(end).avgData = fiveDayAvg;
+            fiveDayMax(end).max= max([fiveDayAvg.avg]);
         end
     end
-end
-fiveDayMax = struct("middle", {}, "interval", {},"max", {});
-newinterval = true;
-dayCounter = 0;
-for i=1:size(tt, 1)
-    if tt(i,2).precipitation < 0
-        tt(i,2).precipitation = 0;
-    end
-    if newinterval == true
-        startDate = tt(i,1).adjustedTime;
-        currentDay = day(tt(i,1).adjustedTime);
-        fiveDayTable = table([],[],'VariableNames',["days", "precipitation"]);
-        precipitationTotal = [tt(i,2).precipitation];
-        newinterval = false;
-    elseif dayCounter < 5 && currentDay == day(tt(i,1).adjustedTime)
-        precipitationTotal(end+1) = tt(i,2).precipitation;
-    elseif dayCounter < 5
-        currentDay = day(tt(i,1).adjustedTime);
-        dayCounter = dayCounter + 1;
-        fiveDayTable = [fiveDayTable; {tt(i,1).adjustedTime-days(1), mean(precipitationTotal)}];
-        precipitationTotal = [tt(i,2).precipitation];
-    elseif dayCounter == 5
-        dayCounter = 0;
-        fiveDayMax(end+1).middle = startDate+days(2);
-        fiveDayMax(end).interval = fiveDayTable;
-        fiveDayMax(end).max = max(fiveDayTable.precipitation);
-        precipitationTotal(end+1) = tt(i,2).precipitation;
-        %precipitationTotal = precipitationTotal+ tt(i,2).precipitation;
-        fiveDayTable = table([],[],'VariableNames',["days", "precipitation"]);
-        startDate = tt(i-1,1).adjustedTime;
-    end
-end
 
-monthMax = struct("month", {}, "maxData", {}, "median", {});
-monthData = [];
-monthNames = [];
-newTable = true;
-for i=1:12
-    for j=1:size(fiveDayMax,2)
-        if isnan(fiveDayMax(1,j).max)
-            continue
-        elseif month(fiveDayMax(j).middle) == i
-            if newTable == true
-                currentMax = table([fiveDayMax(1,j).middle],[fiveDayMax(1,j).max],'VariableNames',["days", "max"]);
-                newTable = false;
+    monthMax = struct("month", {}, "maxData", {}, "median", {});
+    monthData = [];
+    monthNames = [];
+    newTable = true;
+    for i=1:12
+        for j=1:size(fiveDayMax,2)
+            if isnan(fiveDayMax(1,j).max)
+                continue
+            elseif month(fiveDayMax(j).middle) == i
+                if newTable == true
+                    currentMax = table([fiveDayMax(1,j).middle],[fiveDayMax(1,j).max],'VariableNames',["days", "max"]);
+                    newTable = false;
+                else
+                    currentMax = [currentMax; {fiveDayMax(1,j).middle,fiveDayMax(1,j).max}];
+                end
+            end
+        end
+        monthMax(end+1).month = datetime(2000,i,1,'format','MMM');
+        monthMax(end).maxData = currentMax;
+        monthMax(end).median = median(currentMax.max);
+        %  monthData = [monthData; currentMax.max];
+        % monthNames = [monthNames; repmat({string(i)},size(currentMax,1),1)];
+        newTable = true;
+    end
+    for i=1:size(mooringValues,2)
+        if mooringValues(1,i).lat == precLat && mooringValues(1,i).lon == precLon
+            [M, I] = max([monthMax.median]);
+            if contains(fileName, '10m')
+                mooringValues(i).maxRain_10m= M;
+                mooringValues(i).minRain_10m = min([monthMax.median]);
+                mooringValues(i).rainRatio_10m = max([monthMax.median])/min([monthMax.median]);
+                mooringValues(i).maxrainMonth_10m = I;
             else
-                currentMax = [currentMax; {fiveDayMax(1,j).middle,fiveDayMax(1,j).max}];
+                mooringValues(i).maxRain_hr= M;
+                mooringValues(i).minRain_hr = min([monthMax.median]);
+                mooringValues(i).rainRatio_hr = max([monthMax.median])/min([monthMax.median]);
+                mooringValues(i).maxrainMonth_hr = I;
             end
         end
     end
-    monthMax(end+1).month = datetime(2000,i,1,'format','MMM');
-    monthMax(end).maxData = currentMax;
-    monthMax(end).median = median(currentMax.max);
-    monthData = [monthData; currentMax.max];
-    monthNames = [monthNames; repmat({string(i)},size(currentMax,1),1)];
-    newTable = true;
 end
+%end
+%boxplot(monthData, monthNames)
+%xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
+%     graphname = 'graphs2\'+string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
+%     savefig(graphname);
 
-boxplot(monthData, monthNames)
-xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
-graphname = 'graphs2\'+string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
-savefig(graphname);
 
+%save('values2.mat', mooringValues);
+
+function fileList = openFiles(folder, targetFolder)
+unzip(folder, targetFolder);
+gunzip(string(targetFolder)+'/*.gz'); % decompress files
+delete(string(targetFolder)+'/*.gz'); % delete compressed files
+fileList = dir(fullfile(targetFolder,'*.cdf'));
+end
--- seasalinitybymonth.m
+++ seasalinitybymonth.m
@@ -1,96 +1,111 @@
-%rainFiles = openFiles('rain.zip','rainfiles');
 sssFiles = openFiles('sss.zip','sssfiles');
 
-%for file = {sssFiles.name}
-%fileName = 'sssfiles\' +string(file{1});
-fileName = 'sssfiles\sss0n0e_hr.cdf';
-timeData = ncread(fileName,'time');
-salinityData = ncread(fileName,'S_41');
-ssValues = salinityData(:,:,1,:);
-salinity = squeeze(ssValues); % puts salinity values in 1-D array
+mooringValues = struct('lat',{},'lon',{},'maximum',{},'minimum',{},'ratio',{});
 
-timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
-startTime = erase(timeDescrip, 'hours since '); % strips words
-startTime = datetime(startTime);
-startMonth = month(startTime);
-startYear = year(startTime);
+for file = {sssFiles.name}
+    fileName = 'sssfiles\' +string(file{1});
+    %fileName = 'sssfiles\sss0n35w_hr.cdf';
+    timeData = ncread(fileName,'time');
+    salinityData = ncread(fileName,'S_41');
+    mooringValues(end+1).lat = ncread(fileName,'lat');
+    mooringValues(end).lon = ncread(fileName,'lon');
+    ssValues = salinityData(:,:,1,:);
+    salinity = squeeze(ssValues); % puts salinity values in 1-D array
 
-adjustedTime = dateshift(startTime, 'start', 'hour', timeData); % calculate dates based on hours
-tt = timetable(adjustedTime, salinity); % table with times and salinity values
+    timeDescrip = ncreadatt(fileName, 'time','units'); % reads in start time
+    startTime = erase(timeDescrip, 'hours since '); % strips words
+    startTime = datetime(startTime);
+    startMonth = month(startTime);
+    startYear = year(startTime);
 
-numDays = timeData(end)/24;
-intervalStart = datetime(year(startTime),month(startTime),day(startTime)+1,0,0,0);
-intervals = intervalStart + caldays(0:5:numDays);
+    adjustedTime = dateshift(startTime, 'start', 'hour', timeData); % calculate dates based on hours
+    tt = timetable(adjustedTime, salinity); % table with times and salinity values
 
-fiveDayStv = struct("start", {}, "intervalData", {},"stv", {},"percmissing", {});
+    numDays = timeData(end)/24;
+    intervalStart = datetime(year(startTime),month(startTime),day(startTime)+1,0,0,0);
+    intervals = intervalStart + caldays(0:5:numDays);
 
-for m=1:size(intervals,2)-1 % dividing the month up into 5-day intervals
-    intervalRange = timerange(intervals(1,m),intervals(1,m+1)-caldays(1),'days');
-    fiveDayInterval = tt(intervalRange,:);
-    fiveDayStv(end+1).start = intervals(1,m) + days(2) + hours(12);
-    NaNCounter = 0;
-    for i=1:size(fiveDayInterval,1)
-        if isnan(fiveDayInterval(i,1).salinity)
-            NaNCounter = NaNCounter + 1;
-        end
-    end
-    percNaN = NaNCounter/size(fiveDayInterval,1);
-    fiveDayStv(end).percmissing = percNaN;
-    if percNaN > 0.20
-        fiveDayStv(end).intervalData = fiveDayInterval;
-    else
-        fiveDayInterval=rmmissing(fiveDayInterval); %remove missing data
-        fiveDayStv(end).intervalData = fiveDayInterval;
-    end
-    fiveDayStv(end).stv = std(fiveDayInterval.salinity);
-end
+    fiveDayStv = struct("start", {}, "intervalData", {},"stv", {},"percmissing", {});
 
-monthStv = struct("month", {}, "stvData", {}, "median", {});
-monthData = [];
-monthNames = [];
-% for i=1:12
-%     disp(i)
-% end
-newTable = true;
-for i=1:12
-    for j=1:size(fiveDayStv,2)
-        if isnan(fiveDayStv(1,j).stv) 
-            continue
-        elseif month(fiveDayStv(j).start) == i
-            if newTable == true
-                currentStv = table([fiveDayStv(1,j).start],[fiveDayStv(1,j).stv],'VariableNames',["days", "stv"]);
-                newTable = false;
-            else
-                currentStv = [currentStv; {fiveDayStv(1,j).start,fiveDayStv(1,j).stv}];
+    for m=1:size(intervals,2)-1 % dividing the month up into 5-day intervals
+        intervalRange = timerange(intervals(1,m),intervals(1,m+1)-caldays(1),'days');
+        fiveDayInterval = tt(intervalRange,:);
+        fiveDayStv(end+1).start = intervals(1,m) + days(2) + hours(12);
+        NaNCounter = 0;
+        for i=1:size(fiveDayInterval,1)
+            if isnan(fiveDayInterval(i,1).salinity)
+                NaNCounter = NaNCounter + 1;
             end
         end
+        percNaN = NaNCounter/size(fiveDayInterval,1);
+        fiveDayStv(end).percmissing = percNaN;
+        if percNaN > 0.20
+            fiveDayStv(end).intervalData = fiveDayInterval;
+        else
+            fiveDayInterval=rmmissing(fiveDayInterval); %remove missing data
+            fiveDayStv(end).intervalData = fiveDayInterval;
+        end
+        fiveDayStv(end).stv = std(fiveDayInterval.salinity);
     end
-    monthStv(end+1).month = datetime(2000,i,1,'format','MMM');
-    monthStv(end).stvData = currentStv;
-    monthStv(end).median = median(currentStv.stv);
-    monthData = [monthData; currentStv.stv];
-    monthNames = [monthNames; repmat({string(i)},size(currentStv,1),1)];
+
+    monthStv = struct("month", {}, "stvData", {}, "median", {});
+    monthData = [];
+    monthNames = [];
+    % for i=1:12
+    %     disp(i)
+    % end
     newTable = true;
+    for i=1:12
+        for j=1:size(fiveDayStv,2)
+            if isnan(fiveDayStv(1,j).stv)
+                continue
+            elseif month(fiveDayStv(j).start) == i
+                if newTable == true
+                    currentStv = table([fiveDayStv(1,j).start],[fiveDayStv(1,j).stv],'VariableNames',["days", "stv"]);
+                    newTable = false;
+                else
+                    currentStv = [currentStv; {fiveDayStv(1,j).start,fiveDayStv(1,j).stv}];
+                end
+            end
+        end
+        monthStv(end+1).month = datetime(2000,i,1,'format','MMM');
+        monthStv(end).stvData = currentStv;
+        monthStv(end).median = median(currentStv.stv);
+        monthData = [monthData; currentStv.stv];
+        monthNames = [monthNames; repmat({string(i)},size(currentStv,1),1)];
+        newTable = true;
+    end
 end
-
-
-bar([monthStv.median])
-xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
-xlabel('Time(months)')
-ylabel('Median std sea surface salinity for 5-day intervals')
-title('Salinity over months')
-
 boxplot(monthData, monthNames)
 xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
+graphname = 'graphs2\'+string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
+xlabel('Time (months)')
+ylabel('sss')
+title('Mooring location '+ string(extractBetween(fileName,'sssfiles\sss','_hr.cdf')));
+savefig(graphname);
+%     [M, I] = max([monthStv.median]);
+%     mooringValues(end).maximum= M;
+%     mooringValues(end).minimum = min([monthStv.median]);
+%     mooringValues(end).ratio = max([monthStv.median])/min([monthStv.median]);
+%     mooringValues(end).maxMonth = I;
+%end
+% bar([monthStv.median])
+% xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
+% xlabel('Time(months)')
+% ylabel('Median std sea surface salinity for 5-day intervals')
+% title('Salinity over months')
 
-% graphname = string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
+% boxplot(monthData, monthNames)
+% xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'})
+% graphname = 'graphs2\'+string(extractBetween(fileName,'sssfiles\','.cdf')) + 'months.fig';
 % savefig(graphname);
+
 % fileName = 'output\'+ erase(fileName,'months.cdf') + '.mat';
 % save(fileName,"fiveDayStv","monthStv")
 
 function fileList = openFiles(folder, targetFolder)
-    unzip(folder, targetFolder);
-    gunzip(string(targetFolder)+'/*.gz'); % decompress files
-    delete(string(targetFolder)+'/*.gz'); % delete compressed files
-    fileList = dir(fullfile(targetFolder,'*.cdf'));
+unzip(folder, targetFolder);
+gunzip(string(targetFolder)+'/*.gz'); % decompress files
+delete(string(targetFolder)+'/*.gz'); % delete compressed files
+fileList = dir(fullfile(targetFolder,'*.cdf'));
 end
